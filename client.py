3622cc9ebe3cffaa914b1786f18ba8ebe187fbc9
jayll0 <aryawijaya6664@gmail.com>
2025-05-15 16:06:13 +0700

    p

--- client.py
+++ client.py
@@ -1,9 +1,9 @@
 import socket
 
-serverName='servername'
+serverName='localhost'
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serverPort=12000
-clientSocket.connect(('',serverPort))
+clientSocket.connect((serverName,serverPort))
 message = "Halo dari client 103012330330"
 clientSocket.send(message.encode())
 response = clientSocket.recv(1024).decode()
